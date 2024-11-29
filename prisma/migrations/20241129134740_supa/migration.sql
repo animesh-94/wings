5ba1e7a54a43488e9c1cbb75f2009955441d6255
screenager <tmahajan09876@gmail.com>
2024-11-29 19:25:49 +0530

    add: supa time

--- prisma/migrations/20241129134740_supa/migration.sql
+++ prisma/migrations/20241129134740_supa/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "Visitor" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "college" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "verified" BOOLEAN NOT NULL DEFAULT false
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Visitor_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
